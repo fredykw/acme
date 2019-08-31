165b8f10584ca0c70be4adcfe474a8f4ef8419b1
fredykw <fredykarayil@gmail.com>
2019-08-31 12:27:40 +0530

    Fixes #5: Created models for all tables and also country controller

---
+++ models/Country.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "country".
+ *
+ * @property string $name
+ */
+class Country extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'country';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'string', 'max' => 10],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'name' => 'Name',
+        ];
+    }
+}
